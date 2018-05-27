069b1a81037575850847f5a0928ceb206c2f3e34
CHEF\Ksusha-plusha <>
2018-05-27 16:41:25 +0700

    first commit

---
+++ SpeechRecognition/SpeechRecognitionForm.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace SpeechRecognition
+{
+    partial class SpeechRecognitionForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbWorkspace = new System.Windows.Forms.ListBox();
+            this.btnEnable = new System.Windows.Forms.Button();
+            this.btnDisable = new System.Windows.Forms.Button();
+            this.lbColor = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lbWorkspace
+            // 
+            this.lbWorkspace.FormattingEnabled = true;
+            this.lbWorkspace.ItemHeight = 16;
+            this.lbWorkspace.Location = new System.Drawing.Point(50, 49);
+            this.lbWorkspace.Name = "lbWorkspace";
+            this.lbWorkspace.Size = new System.Drawing.Size(365, 196);
+            this.lbWorkspace.TabIndex = 0;
+            // 
+            // btnEnable
+            // 
+            this.btnEnable.Location = new System.Drawing.Point(444, 162);
+            this.btnEnable.Name = "btnEnable";
+            this.btnEnable.Size = new System.Drawing.Size(100, 30);
+            this.btnEnable.TabIndex = 1;
+            this.btnEnable.Text = "Enable";
+            this.btnEnable.UseVisualStyleBackColor = true;
+            this.btnEnable.Click += new System.EventHandler(this.btnEnable_Click);
+            // 
+            // btnDisable
+            // 
+            this.btnDisable.Location = new System.Drawing.Point(444, 216);
+            this.btnDisable.Name = "btnDisable";
+            this.btnDisable.Size = new System.Drawing.Size(100, 29);
+            this.btnDisable.TabIndex = 2;
+            this.btnDisable.Text = "Disable";
+            this.btnDisable.UseVisualStyleBackColor = true;
+            this.btnDisable.Click += new System.EventHandler(this.btnDisable_Click);
+            // 
+            // lbColor
+            // 
+            this.lbColor.AutoSize = true;
+            this.lbColor.Location = new System.Drawing.Point(441, 49);
+            this.lbColor.Name = "lbColor";
+            this.lbColor.Size = new System.Drawing.Size(92, 17);
+            this.lbColor.TabIndex = 3;
+            this.lbColor.Text = "Change color";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(444, 85);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // SpeechRecognitionForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(607, 297);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.lbColor);
+            this.Controls.Add(this.btnDisable);
+            this.Controls.Add(this.btnEnable);
+            this.Controls.Add(this.lbWorkspace);
+            this.Name = "SpeechRecognitionForm";
+            this.Text = "SpeechRecognition";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lbWorkspace;
+        private System.Windows.Forms.Button btnEnable;
+        private System.Windows.Forms.Button btnDisable;
+        private System.Windows.Forms.Label lbColor;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ SpeechRecognition/SpeechRecognitionForm.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Windows.Forms;
+using Microsoft.Speech.Recognition;
+using System.Globalization;
+
+namespace SpeechRecognition
+{
+    public partial class SpeechRecognitionForm : Form
+    {
+        /// <summary>
+        /// Указывается распознаваемый язык, в данном случае United States English
+        /// </summary>
+        static CultureInfo cultureInfo = new CultureInfo("en-us");
+        /// <summary>
+        /// Позволяет приложению прослушивать и распознавать произносимые слова или фразы
+        /// </summary>
+        static SpeechRecognitionEngine speechRecognition = new SpeechRecognitionEngine(cultureInfo);
+
+        public SpeechRecognitionForm()
+        {
+            InitializeComponent();
+
+            speechRecognition.SetInputToDefaultAudioDevice();
+            speechRecognition.SpeechRecognized += SpeechRecognition_SpeechRecognized;
+
+            Grammar g_HelloGoodbye = HelloGoodbyeGrammar();
+            Grammar g_ColorChange = ChangeColorGrammar();
+            Grammar g_Number = GetAmountGrammar();
+
+            speechRecognition.LoadGrammarAsync(g_HelloGoodbye);
+            speechRecognition.LoadGrammarAsync(g_ColorChange);
+            speechRecognition.LoadGrammarAsync(g_Number);
+        }
+
+        /// <summary>
+        /// Обработчик событий SpeechRecognized 
+        /// </summary>
+        private void SpeechRecognition_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
+        {
+            string txt = e.Result.Text;
+            float conf = e.Result.Confidence;
+
+            if (conf < 0.65) return;
+
+            this.Invoke(new MethodInvoker(() =>
+            {
+                if (txt.IndexOf("good morning") >= 0)
+                {
+                    lbWorkspace.Items.Add("Hello, Ksu! How are you?");
+                }
+
+                if (txt.IndexOf("goodbye") >= 0)
+                {
+                    lbWorkspace.Items.Add("GoodBye!");
+                }
+            }));
+
+            this.Invoke(new MethodInvoker(() =>
+            {
+                if (txt.IndexOf("change") >= 0 && txt.IndexOf("green") >= 0)
+                {
+                    this.Invoke(new MethodInvoker(() =>
+                    {
+                        pictureBox1.BackColor = Color.Green;
+                    }));
+                }
+
+                if (txt.IndexOf("change") >= 0 && txt.IndexOf("blue") >= 0)
+                {
+                    this.Invoke(new MethodInvoker(() =>
+                    {
+                        pictureBox1.BackColor = Color.Blue;
+                    }));
+                }
+
+                if (txt.IndexOf("change") >= 0 && txt.IndexOf("red") >= 0)
+                {
+                    this.Invoke(new MethodInvoker(() =>
+                    {
+                        pictureBox1.BackColor = Color.Red;
+                    }));
+                }
+
+                if (txt.IndexOf("change") >= 0 && txt.IndexOf("yellow") >= 0)
+                {
+                    this.Invoke(new MethodInvoker(() =>
+                    {
+                        pictureBox1.BackColor = Color.Yellow;
+                    }));
+                }
+            }));
+
+            this.Invoke(new MethodInvoker(() =>
+            {
+                if (txt.IndexOf("amount") >= 0 && txt.IndexOf("yes") >= 0)
+                {
+                    string[] words = txt.Split(' ');
+                    int num1 = int.Parse(words[1]);
+                    int num2 = int.Parse(words[3]);
+                    int sum = num1 + num2;
+                    lbWorkspace.Items.Add("The amount  = " + num1 + " + " + num2 + " = "+ sum);
+                }
+            }));
+        }
+
+        /// <summary>
+        /// Метод, включающий распознавание речи
+        /// </summary>
+        private void btnEnable_Click(object sender, EventArgs e)
+        {
+            speechRecognition.RecognizeAsync(RecognizeMode.Multiple);
+            btnDisable.Enabled = true;
+        }
+
+        /// <summary>
+        /// Метод, выключающий распознавание речи
+        /// </summary>
+        private void btnDisable_Click(object sender, EventArgs e)
+        {
+            speechRecognition.RecognizeAsyncStop();
+            btnDisable.Enabled = false;
+        }
+
+        /// <summary>
+        /// Метод, настраивающий возможность распознавания команд для приветствия и прощания
+        /// </summary>
+        static Grammar HelloGoodbyeGrammar()
+        {
+            Choices ch_HelloGoodbye = new Choices();
+            ch_HelloGoodbye.Add("good morning");
+            ch_HelloGoodbye.Add("goodbye");
+            GrammarBuilder gb_result =
+              new GrammarBuilder(ch_HelloGoodbye);
+            Grammar g_result = new Grammar(gb_result);
+            return g_result;
+        }
+
+        /// <summary>
+        /// Метод, настраивающий возможность распознавания цвета изменения pictureBox
+        /// </summary>        
+        static Grammar ChangeColorGrammar()
+        {
+            Choices ch_Colors = new Choices();
+            ch_Colors.Add(new string[] { "green", "blue", "red", "yellow" });
+            GrammarBuilder gb_result = new GrammarBuilder();
+            gb_result.Append("change color");
+            gb_result.Append(ch_Colors);
+            Grammar g_result = new Grammar(gb_result);
+            return g_result;
+        }
+
+        /// <summary>
+        /// Метод, настраивающий возможность распознавание цифр для сложения
+        /// </summary>
+        static Grammar GetAmountGrammar()
+        {
+            Choices ch_Numbers = new Choices();
+            ch_Numbers.Add("1");
+            ch_Numbers.Add("2");
+            ch_Numbers.Add("3");
+            ch_Numbers.Add("4");
+
+            GrammarBuilder gb_WhatIsXplusY = new GrammarBuilder();
+            gb_WhatIsXplusY.Append("amount");
+            gb_WhatIsXplusY.Append(ch_Numbers);
+            gb_WhatIsXplusY.Append("yes");
+            gb_WhatIsXplusY.Append(ch_Numbers);
+            Grammar g_result = new Grammar(gb_WhatIsXplusY);
+
+            return g_result;
+        }
+    }
+}
